1058c6cbb3a9cc8a5c962102f96c3017836687de
Dustin Craig <dustin@dustinccraig.com>
2020-05-31 21:01:58 -0400

    Started landing page

---
+++ src/components/landing/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Image from '../../assets/images/Landing.png'
+import './landing.css'
+
+export class Landing extends Component {
+    constructor(props) {
+        super(props) 
+    }
+
+    render() {
+        return (
+            <div className="LandingContainer" style={{backgroundImage: `url(${Image})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundAttachment: 'fixed', backgroundRepeat: 'no-repeat'}}>
+                <h2 className="LandingHeader">Dustin Craig</h2>   
+                <h3 className="LandingSubHeader">Software Engineer</h3>
+           </div>
+        )
+    }
+}
