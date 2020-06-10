175a50283259f1076045873c2986be4d7a9482ee
Dustin Craig <dustin@dustinccraig.com>
2020-06-09 22:20:30 -0400

    container creation

---
+++ src/components/container/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../../styles/main.css'
+
+
+export class Container extends Component {
+
+    render() {
+        return (
+            <div className="container">
+
+            </div>
+        )
+    }
+}
--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
 import { SocialButtons } from '../socialButtons'
 import { SideBar } from '../sideBar'
+import { Container } from '../container'
 import Image from '../../assets/images/Landing.png'
 import './landing.css'
 
 export class Landing extends Component {
     constructor(props) {
         super(props) 
+        this.sideBarItemClick.bind(this)
+
+        this.state = {
+            experienceOpen: false, 
+            aboutOpen: false, 
+            projectsOpen: false, 
+            contactOpen: false 
+        }
+    }
+
+    sideBarItemClick = (item) => {
+        switch(item) {
+            case 'experience':
+                this.setState({experienceOpen: !this.state.experienceOpen})
+                break 
+            case 'about':
+                this.setState({aboutOpen: !this.state.aboutOpen})
+                break 
+            case 'projects':
+                this.setState({projectsOpen: !this.state.projectsOpen})
+                break 
+            case 'contact':
+                this.setState({contactOpen: !this.state.contactOpen})
+        }
     }
 
     render() {
@@ -15,7 +40,7 @@ export class Landing extends Component {
                 <SideBar />
                 <h2 className="LandingHeader">Dustin Craig</h2>   
                 <h3 className="LandingSubHeader">Software Engineer</h3>
-                
+                <Container />
                 <footer className="SocialButtonsContainer"> 
                     <SocialButtons />
                 </footer>
