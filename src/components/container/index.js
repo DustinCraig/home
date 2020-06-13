4b74e69167c70c29b435c6b8d5c93128b0250c6a
Dustin Craig <dustincraig@Dustins-MacBook-Pro.local>
2020-06-12 21:23:00 -0400

    Experience card updates

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import { ExperienceCard } from './experienceCard'
 import '../../styles/main.css'
 
 export class Container extends Component {
-    render() {
-        return (
-            <div className='containerContent'>
-                <div className='heading'>{this.props.heading}</div>
-            </div>
-        )
-    }
+  renderExperience() {
+    return <ExperienceCard title={'FLIR Detection'} />
+  }
+
+  render() {
+    return (
+      <div className='containerContent'>
+        <div className='heading'>{this.props.heading}</div>
+        {this.props.heading === 'Experience' && this.renderExperience()}
+      </div>
+    )
+  }
 }
--- src/components/landing/index.js
+++ src/components/landing/index.js
@@ -10,157 +10,152 @@ import Image from '../../assets/images/Landing.png'
 import './landing.css'
 
 export class Landing extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            homeOpen: true,
-            experienceOpen: false,
-            aboutOpen: false,
-            projectsOpen: false,
-            contactOpen: false,
-            sideBarOpen: true,
-        }
-        this.updateDimensions.bind(this)
+    this.state = {
+      homeOpen: true,
+      experienceOpen: false,
+      aboutOpen: false,
+      projectsOpen: false,
+      contactOpen: false,
+      sideBarOpen: true,
     }
+    this.updateDimensions.bind(this)
+  }
 
-    componentDidMount() {
-        window.addEventListener('resize', this.updateDimensions)
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions)
+  }
+
+  updateDimensions = () => {
+    if (window.innerWidth < HIDESIDEBARWIDTH)
+      this.setState({ sideBarOpen: false })
+    else this.setState({ sideBarOpen: true })
+  }
+
+  sideBarItemClick = (item) => {
+    switch (item) {
+      case 'Home':
+        this.setState({
+          homeOpen: true,
+          experienceOpen: false,
+          aboutOpen: false,
+          projectsOpen: false,
+          contactOpen: false,
+        })
+        break
+      case 'Experience':
+        this.setState({
+          homeOpen: false,
+          experienceOpen: true,
+          aboutOpen: false,
+          projectsOpen: false,
+          contactOpen: false,
+        })
+        break
+      case 'About':
+        this.setState({
+          homeOpen: false,
+          experienceOpen: false,
+          aboutOpen: true,
+          projectsOpen: false,
+          contactOpen: false,
+        })
+        break
+      case 'Projects':
+        this.setState({
+          homeOpen: false,
+          experienceOpen: false,
+          aboutOpen: false,
+          projectsOpen: true,
+          contactOpen: false,
+        })
+        break
+      case 'Contact':
+        this.setState({
+          homeOpen: false,
+          experienceOpen: false,
+          aboutOpen: false,
+          projectsOpen: false,
+          contactOpen: true,
+        })
+        break
+      default:
+        break
     }
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.updateDimensions)
-    }
+  render() {
+    return (
+      <div
+        className='LandingContainer'
+        style={{
+          backgroundImage: `url(${Image})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundAttachment: 'fixed',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        {!this.state.sideBarOpen && (
+          <div className='hamburgerIconContainer'>
+            {/* For some reason this has to be styled inline? */}
+            <FontAwesomeIcon style={{ width: 40, height: 40 }} icon={faBars} />
+          </div>
+        )}
 
-    updateDimensions = () => {
-        if (window.innerWidth < HIDESIDEBARWIDTH)
-            this.setState({ sideBarOpen: false })
-        else this.setState({ sideBarOpen: true })
-    }
-
-    sideBarItemClick = (item) => {
-        switch (item) {
-            case 'Home':
-                this.setState({
-                    homeOpen: true,
-                    experienceOpen: false,
-                    aboutOpen: false,
-                    projectsOpen: false,
-                    contactOpen: false,
-                })
-                break
-            case 'Experience':
-                this.setState({
-                    homeOpen: false,
-                    experienceOpen: true,
-                    aboutOpen: false,
-                    projectsOpen: false,
-                    contactOpen: false,
-                })
-                break
-            case 'About':
-                this.setState({
-                    homeOpen: false,
-                    experienceOpen: false,
-                    aboutOpen: true,
-                    projectsOpen: false,
-                    contactOpen: false,
-                })
-                break
-            case 'Projects':
-                this.setState({
-                    homeOpen: false,
-                    experienceOpen: false,
-                    aboutOpen: false,
-                    projectsOpen: true,
-                    contactOpen: false,
-                })
-                break
-            case 'Contact':
-                this.setState({
-                    homeOpen: false,
-                    experienceOpen: false,
-                    aboutOpen: false,
-                    projectsOpen: false,
-                    contactOpen: true,
-                })
-                break
-            default:
-                break
-        }
-    }
-
-    render() {
-        return (
-            <div
-                className='LandingContainer'
-                style={{
-                    backgroundImage: `url(${Image})`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                    backgroundAttachment: 'fixed',
-                    backgroundRepeat: 'no-repeat',
-                }}
-            >
-                {!this.state.sideBarOpen && (
-                    <div className='hamburgerIconContainer'>
-                        {/* For some reason this has to be styled inline? */}
-                        <FontAwesomeIcon
-                            style={{ width: 40, height: 40 }}
-                            icon={faBars}
-                        />
-                    </div>
-                )}
-
-                {this.state.sideBarOpen && (
-                    <React.Fragment>
-                        <div className='containerLeft'>
-                            <SideBar sideBarItemClick={this.sideBarItemClick} />
-                        </div>
-                    </React.Fragment>
-                )}
-                <div
-                    className={`containerRight ${
-                        this.state.homeOpen === true ? 'easeIn' : 'easeOut'
-                    }`}
-                >
-                    <ContainerCentered
-                        heading={'Dustin Craig'}
-                        subHeading={'Software Engineer'}
-                        footer={<SocialButtons />}
-                    />
-                </div>
-                <div
-                    className={`containerRight ${
-                        this.state.experienceOpen === true
-                            ? 'easeIn'
-                            : 'easeOut'
-                    }`}
-                >
-                    <Container heading={'Experience'} />
-                </div>
-                <div
-                    className={`containerRight ${
-                        this.state.aboutOpen === true ? 'easeIn' : 'easeOut'
-                    }`}
-                >
-                    <Container heading={'About'} />
-                </div>
-                <div
-                    className={`containerRight ${
-                        this.state.projectsOpen === true ? 'easeIn' : 'easeOut'
-                    }`}
-                >
-                    <Container heading={'Projects'} />
-                </div>
-                <div
-                    className={`containerRight ${
-                        this.state.contactOpen === true ? 'easeIn' : 'easeOut'
-                    }`}
-                >
-                    <Container heading={'Contact'} />
-                </div>
+        {this.state.sideBarOpen && (
+          <React.Fragment>
+            <div className='containerLeft'>
+              <SideBar sideBarItemClick={this.sideBarItemClick} />
             </div>
-        )
-    }
+          </React.Fragment>
+        )}
+        <div
+          className={`containerRight ${
+            this.state.homeOpen === true ? 'easeIn' : 'easeOut'
+          }`}
+        >
+          <ContainerCentered
+            heading={'Dustin Craig'}
+            subHeading={'Software Engineer'}
+            footer={<SocialButtons />}
+          />
+        </div>
+        <div
+          className={`containerRight ${
+            this.state.experienceOpen === true ? 'easeIn' : 'easeOut'
+          }`}
+        >
+          <Container heading={'Experience'} />
+        </div>
+        <div
+          className={`containerRight ${
+            this.state.aboutOpen === true ? 'easeIn' : 'easeOut'
+          }`}
+        >
+          <Container heading={'About'} />
+        </div>
+        <div
+          className={`containerRight ${
+            this.state.projectsOpen === true ? 'easeIn' : 'easeOut'
+          }`}
+        >
+          <Container heading={'Projects'} />
+        </div>
+        <div
+          className={`containerRight ${
+            this.state.contactOpen === true ? 'easeIn' : 'easeOut'
+          }`}
+        >
+          <Container heading={'Contact'} />
+        </div>
+      </div>
+    )
+  }
 }
